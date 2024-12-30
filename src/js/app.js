28a3b787e4421f9a952434b57fba44764f1ba0a7
AlmiraGayaz <almiragayazetdinova@gmail.com>
2024-12-30 22:28:29 +0500

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+const timer = (count) => {
+    console.log("Запускаем таймер");
+    for (let i = 1; i <= count; i++) {
+      alert(i);
+    }
+    console.log("Обратный отсчет закончен");
+  };
+  
+  timer(5);

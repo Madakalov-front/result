f6ed8a6be31e4fb9c2af510231aeb7ce16f20cb2
Egor Madakalov <gpt.e.madakalov@gmail.com>
2025-01-13 23:37:16 +0500

    functions

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,21 @@
 // import { checkTemp, getHP, salary, square } from "./week_2/debugg/debugger.js";
 
+import { getDivisorsCount, getSumOfNumbers, setGetName } from "./week_2/functions/functions.js";
+
 // Debugger
 // getHP()
 // checkTemp()
 // salary()
 // square()
+
+// Functions
+// setGetName()
+// console.log(getSumOfNumbers('1asd')) // NaN
+// console.log(getSumOfNumbers(10, "odd")); // 25
+// console.log(getSumOfNumbers(10, "even")); // 30
+// console.log(getSumOfNumbers(10, "")); // 55
+// getDivisorsCount(1.2) // alert
+console.log(getDivisorsCount(4)); // Вернет 3 (делители - 1, 2, 4)
+console.log(getDivisorsCount(5)); // Вернет 2 (делители - 1, 5)
+console.log(getDivisorsCount(12)); // Вернет 6 (делители - 1, 2, 3, 4, 6, 12)
+console.log(getDivisorsCount(30)); // Вернет 8 (делители - 1, 2, 3, 5, 6, 10, 15, 30)
---
+++ src/js/week_2/functions/functions.js
@@ -0,0 +1,56 @@
+export const setGetName = () => {
+    function getName1(name) {
+        return `Имя равно ${name}`;
+    }
+
+    const getName2 = function (name) {
+        return `Имя равно ${name}`;
+    };
+
+    const getName3 = (name) => `Имя равно ${name}`;
+    console.log(getName1("Tom"), " - getName1");
+    console.log(getName2("Tom"), " - getName2");
+    console.log(getName3("Tom"), " - getName3");
+};
+
+export const getSumOfNumbers = (number, type = "odd") => {
+
+    if (!number || Number.isNaN(Number(number))) return NaN;
+
+    let count = null;
+
+    for (let i = 0; i <= number; i++) {
+        if (type === "odd") {
+            if (i % 2 !== 0) {
+                count += i;
+            }
+        } else if (type === "even") {
+            if (i % 2 === 0) {
+                count += i;
+                continue;
+            }
+        } else if (type === "") {
+            count += i;
+        }
+    }
+
+    return count;
+};
+
+
+export const getDivisorsCount = (number) => {
+    if (!number || Number.isNaN(Number(number))) return NaN;
+    if( number  < 0 || !Number.isInteger(number)) alert(`${number} должен быть целым числом и больше нуля!`);
+    // const divider = []
+    let count = 0;
+
+    for (let i = 1; i <= number; i++) {
+        if(number % i === 0) {
+            count++;
+            // divider.push(i)
+        }
+    }
+
+    // return `Число - ${number}! Делители - ${divider}! Кол-во - ${count}`
+    return count;
+}

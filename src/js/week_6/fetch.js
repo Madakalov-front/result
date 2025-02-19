b99957f8fcb86e02fb1902111c7973ee3d93ecad
Egor Madakalov <gpt.e.madakalov@gmail.com>
2025-02-19 06:29:26 +0500

    fetch

--- src/js/week_6/fetch.js
+++ src/js/week_6/fetch.js
@@ -4,7 +4,7 @@ export const fetchQuest = () => {
     }
 
     const arrayId = () => {
-        return [1, 3, 9, 6, 55]
+        return [1, 3, 9, 6, 7]
     }
     const createItemUser = (nameUser) => {
         console.log(nameUser)
@@ -21,42 +21,17 @@ export const fetchQuest = () => {
         loader().hidden = false;
 
         const URL = 'https://jsonplaceholder.typicode.com/users';
-        const response = await fetch(URL)
 
-        try {
-
-            if (!response.ok) {
-                throw new Error(`Ошибка: ${response.status}`)
-            }
-
-            const users = await response.json();
-
-            if (!Array.isArray(users)) {
-                throw new Error(`Not array: ${users}`)
-            }
-
-            // const listUsers = arrayId().map(item => {
-            //     const user = users.find(user => user.id === item)
-            //     if(!user) return ''
-            //     return createItemUser(user.name)
-            // })
-            // преимуществ перед своим подходом думаю нет, т.к. один запрос идет.
-
-            const listUsers = Promise.all(
-                arrayId().map(id => {
-                    const user = users.find( user => user.id === id)
-                    return user ? createItemUser(user.name) : '';
+        const response = arrayId().map(async id => {
+            return fetch(`${URL}/${id}`).then(response => response.json())
+                .then(data => createItemUser(data.name))
+                .catch(error => console.error(error))
+                .finally(() => {
+                    loader().hidden = true
                 })
-            )
+        })
 
-            return listUsers;
-
-        } catch (error) {
-            console.error("Произошла ошибка:", error.message);
-            return [];
-        } finally {
-            loader().hidden = true
-        }
+        return Promise.all(response);
     }
 
     const renderListUsers = async () => {
@@ -69,53 +44,52 @@ export const fetchQuest = () => {
 
 
 export const fastFetchPhoto = () => {
-    
-const loader = () => {
-    return document.querySelector('#loader');
-}
 
-const arr = () => {
-    return [60, 12, 55, 33, 48, 88];
-}
+    const loader = () => {
+        return document.querySelector('#loader');
+    }
 
-const createPhotoItem = (url, title) => {
-    const li = document.createElement("li");
-    li.classList.add("photo-item");
+    const arr = () => {
+        return [60, 12, 55, 33, 48, 88];
+    }
 
-    const img = document.createElement("img");
-    img.classList.add("photo-item__image");
-    img.src = url;
-    img.alt = title;
+    const createPhotoItem = (url, title) => {
+        const li = document.createElement("li");
+        li.classList.add("photo-item");
 
-    const h3 = document.createElement("h3");
-    h3.classList.add("photo-item__title");
-    h3.textContent = title;
+        const img = document.createElement("img");
+        img.classList.add("photo-item__image");
+        img.src = url;
+        img.alt = title;
 
-    li.appendChild(img);
-    li.appendChild(h3);
+        const h3 = document.createElement("h3");
+        h3.classList.add("photo-item__title");
+        h3.textContent = title;
 
-    return li;
-}
+        li.append(img, h3);
 
-const getFastestLoadedPhoto = async (arr) => {
-    loader().hidden = false;
-    const URL = 'https://api.slingacademy.com/v1/sample-data/photos';
-    const response = arr.map(id => {
-        return fetch(`${URL}/${id}`)
-            .then(response => response.json())
-            .then(data => data.photo)
-            .catch(error => console.error(error))
-            .finally(() => {
-                loader().hidden = true
-            })
-    });
-    return Promise.race(response)
-}
+        return li;
+    }
 
-const renderFastPhoto = async () => {
-    const { title, url } = await getFastestLoadedPhoto(arr())
-    console.log(title, url)
-    document.querySelector('#data-container').appendChild(createPhotoItem(url, title))
-}
-renderFastPhoto()
+    const getFastestLoadedPhoto = async (arr) => {
+        loader().hidden = false;
+        const URL = 'https://api.slingacademy.com/v1/sample-data/photos';
+        const response = arr.map(async id => {
+            return fetch(`${URL}/${id}`)
+                .then(response => response.json())
+                .then(data => data.photo)
+                .catch(error => console.error(error))
+                .finally(() => {
+                    loader().hidden = true
+                })
+        });
+        return Promise.race(response)
+    }
+
+    const renderFastPhoto = async () => {
+        const { title, url } = await getFastestLoadedPhoto(arr())
+        console.log(title, url)
+        document.querySelector('#data-container').appendChild(createPhotoItem(url, title))
+    }
+    renderFastPhoto()
 }

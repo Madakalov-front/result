a53bd3c704bdf55fde1270020a11511564c80975
Egor Madakalov <gpt.e.madakalov@gmail.com>
2025-02-23 14:01:54 +0500

    dop_week_6

---
+++ src/js/week_6/dop.js
@@ -0,0 +1,63 @@
+const getUrlPosts = () => {
+    return `https://jsonplaceholder.typicode.com/posts`;
+}
+const getUrlComments = () => {
+    return `https://jsonplaceholder.typicode.com/comments`;
+}
+
+
+const createElement = ({ tagName = 'div', className = '', textContent = '', parent = null } = {}) => {
+    const elemTitle = document.createElement(tagName);
+    elemTitle.className = className ?? 'null';
+    elemTitle.textContent = textContent ?? 'null';
+    parent?.appendChild(elemTitle)
+    return elemTitle;
+}
+
+const renderCommentPost = (arrayComments) => {
+    const containerComments = createElement({ tagName: 'div', className: 'post-post__comments'})
+    arrayComments.map(comment => {
+        const itemComments = createElement({ tagName: 'div', className: 'post-comment', textContent: '', parent: containerComments })
+        const emailComments = createElement({ tagName: 'span', className: 'post-comment__author', textContent: comment.email, parent: itemComments })
+        const descComments = createElement({ tagName: 'span', className: 'post-comment__text', textContent: comment.body, parent: itemComments })
+    })
+    return containerComments;
+}
+
+const renderPost = async (idPost) => {
+    const elemPost = document.createElement('div');
+    elemPost.id = 'post'
+    elemPost.classList.add('post');
+    try {
+        console.log('start')
+        const responses = await Promise.all([
+            fetch(`${getUrlPosts()}/${idPost}`),
+            fetch(`${getUrlComments()}/?postId=${idPost}`)
+        ])
+
+        if (responses.some(response => !response.ok)) {
+            throw new Error(`Error HTTP: ${responses.find(response => !response.ok).status}`);
+        }
+
+        let [bodyPost, commentsPost] = responses;
+        bodyPost = await bodyPost.json()
+        commentsPost = await commentsPost.json()
+
+        createElement({ tagName: 'h1', className: 'post__title', textContent: bodyPost.title, parent: elemPost });
+        createElement({ tagName: 'p', className: 'post__text', textContent: bodyPost.body, parent: elemPost });
+        createElement({ tagName: 'b', className: 'post__comments-text', textContent: 'Комментарии', parent: elemPost });
+        elemPost.appendChild(renderCommentPost(commentsPost))
+        document.body.append(elemPost)
+
+        return [bodyPost, commentsPost];
+    } catch (error) {
+        console.error(error)
+
+    } finally {
+        console.log('end')
+    }
+}
+
+renderPost(38)
+renderPost(5)
+renderPost(25)

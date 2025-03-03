4d6f006fd0643344290c82a92314b1199a322923
Egor Madakalov <gpt.e.madakalov@gmail.com>
2025-03-03 08:32:41 +0500

    fix

--- finally_project/src/components/List/ListItem.js
+++ finally_project/src/components/List/ListItem.js
@@ -2,9 +2,18 @@ import { Component } from '../../core/Component';
 
 export class ListItem extends Component {
   setup(props) {
+    this.formattedDate = new Date().toLocaleString("ru-RU", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
+  }).replace(/\./g, "/");
+  
     this.$rootElement = document.createElement('div');
     this.$rootElement.className = 'donate-item';
-    this.$rootElement.textContent = '19/01/2024, 18:12:01 -';
+    this.$rootElement.textContent = `${this.formattedDate}  - `;
     const b = document.createElement('b')
     b.textContent = `\u00A0 $${props.sum}`;
     this.$rootElement.append(b)
